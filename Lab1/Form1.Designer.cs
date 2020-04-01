97ac11ab4c772f63f9b46b4c649411bbb37fa714
StockySpark <dan2512@yandex.ru>
2020-04-01 21:05:16 +0300

    Add project files.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.z1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // z1
+            // 
+            this.z1.Location = new System.Drawing.Point(12, 12);
+            this.z1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.z1.Name = "z1";
+            this.z1.ScrollGrace = 0D;
+            this.z1.ScrollMaxX = 0D;
+            this.z1.ScrollMaxY = 0D;
+            this.z1.ScrollMaxY2 = 0D;
+            this.z1.ScrollMinX = 0D;
+            this.z1.ScrollMinY = 0D;
+            this.z1.ScrollMinY2 = 0D;
+            this.z1.Size = new System.Drawing.Size(901, 449);
+            this.z1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(756, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Построить ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 586);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 8;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(57, 503);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(57, 532);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 505);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 534);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 565);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "n";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(57, 561);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 14;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 48);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Решить методом прямоугольников";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 564);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(534, 503);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 48);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Решить методом Ньютона-Лейбница";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(16, 481);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 122);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод прямоугольников";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(534, 565);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(304, 461);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 616);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Интегрирование";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl z1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using System.IO;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        GraphPane pane;
+        bool flag1 = false;
+        bool flag2 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pane = z1.GraphPane;
+        }
+
+        double func(double x)
+        {
+            
+            return Math.Pow(x, 3) - 3 * (Math.Pow(x, 2)) + 8;
+        }
+
+        double primitive(double x)
+        {
+            return (Math.Pow(x, 4) / 4 - Math.Pow(x, 3) + 8 * x);
+        }
+
+        double Integral(double a, double b, int n)
+        {
+            int i;
+            double result, h;
+            result = 0;
+            h = (b - a) / n; 
+            for (i = 0; i < n; i++)
+            {
+                result += func(a + h * (i + 0.5)) * h; 
+            }
+            return result;
+        }
+
+        double NL(double a, double b)
+        {
+            return primitive(b) - primitive(a);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            z1.GraphPane.CurveList.Clear();
+            //GraphPane pane = z1.GraphPane;
+            //pane.Title = "cosX";
+            PointPairList list = new PointPairList();
+            double xmin = /*-1.5*/Convert.ToDouble(numericUpDown1.Value);
+            double xmax = /*3.5*/Convert.ToDouble(numericUpDown2.Value);
+            double xmin_limit = -10;
+            double xmax_limit = 80;
+            double ymin_limit = -1.0;
+            double ymax_limit = 1.0;
+            for (double x = xmin; x <= xmax; x += 0.1)
+            {
+                list.Add(x, func(x)); 
+            }
+            // !!!
+            // Ось X будет пересекаться с осью Y на уровне Y = 0
+            pane.XAxis.Cross = 0.0;
+
+            // Ось Y будет пересекаться с осью X на уровне X = 0
+            pane.YAxis.Cross = 0.0;
+
+            // Отключим отображение первых и последних меток по осям
+            pane.XAxis.Scale.IsSkipFirstLabel = true;
+            pane.XAxis.Scale.IsSkipLastLabel = true;
+
+            // Отключим отображение меток в точке пересечения с другой осью
+            pane.XAxis.Scale.IsSkipCrossLabel = true;
+
+
+            // Отключим отображение первых и последних меток по осям
+            pane.YAxis.Scale.IsSkipFirstLabel = true;
+
+            // Отключим отображение меток в точке пересечения с другой осью
+            pane.YAxis.Scale.IsSkipLastLabel = true;
+            pane.YAxis.Scale.IsSkipCrossLabel = true;
+
+            // Спрячем заголовки осей
+            pane.XAxis.Title.IsVisible = false;
+            pane.YAxis.Title.IsVisible = false;
+            LineItem myCurve = pane.AddCurve("Function", list, Color.Blue, SymbolType.None);
+            //pane.XAxis.Scale.Min = xmin_limit;
+            //pane.XAxis.Scale.Max = xmax_limit;
+            //pane.YAxis.Scale.Min = ymin_limit;
+            //pane.YAxis.Scale.Max = ymax_limit;
+
+            // !!! Установим заливку для кривой
+            // Используем градиентную заливку от красного цвета до голубого через желтый
+            // Последний параметр задает угол наклона градиента
+            myCurve.Line.Fill = new ZedGraph.Fill(Color.Red, Color.Yellow, Color.Blue, 90.0f);
+
+            z1.AxisChange();
+            z1.Invalidate();
+
+            if(flag1 && flag2)
+                label7.Text = "Порядок точности - " + Math.Abs(firstAnswer - secondAnswer);
+        }
+
+        double firstAnswer = 0.0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            firstAnswer = Integral(Convert.ToDouble(numericUpDown1.Value),
+                Convert.ToDouble(numericUpDown2.Value),
+                Convert.ToInt32(numericUpDown3.Value));
+            label5.Text = "Значение интеграла = " + firstAnswer.ToString();
+            flag1 = true;
+        }
+
+        double secondAnswer = 0.0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            secondAnswer = NL(Convert.ToDouble(numericUpDown1.Value), Convert.ToDouble(numericUpDown2.Value));
+            label6.Text = "Значение интеграла = " + secondAnswer;
+            flag2 = true;
+        }
+
+    }
+}
